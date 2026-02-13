b67adfe5d7a69cf1736de0e9f723c2c0d6e24eea
Sultan2403 <sultanremilekun@gmail.com>
2026-02-13 23:53:00 +0100

    added a few mportant details for payment integration

--- Backend/Controllers/payments.controller.js
+++ Backend/Controllers/payments.controller.js
@@ -1,15 +1,21 @@
-const paysatckApiClient = require("../Apis/paystack.api");
+const paystackApiClient = require("../Apis/paystack.api");
 
 const topUp = async (req, res) => {
   const amount = "5000"; /*req.body*/
+  // I'm not sure if amount is a string or number though.
 
   const email = "test@email.com"; /*user.email*/
   // I'll add the normal stuff once the test is complete
 
   // const {amount}  = req.body;
   //const email = user.email
+
+  const Amt_To_Be_Paid = amount * 100;
+
+  // Note the above for dev sanity
+
   try {
-    const response = await paysatckApiClient.post("/transaction/initialize", {
+    const response = await paystackApiClient.post("/transaction/initialize", {
       amount,
       email,
     });
