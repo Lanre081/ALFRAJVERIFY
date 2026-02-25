e08b5c2a1045aa0f3a114426f4fef8b6346af550
Sultan2403 <sultanremilekun@gmail.com>
2026-02-25 16:33:41 +0100

    forceful reset -_-

--- Frontend/src/apis/api.auth.js
+++
@@ -1,8 +0,0 @@
-import api from "./api.client";
-
-const authApi = {
-  login: (payload) => api.post("auth/login", payload),
-  register: (payload) => api.post("auth/register", payload),
-};
-
-export default authApi;
